c99479bc590a9b8343aa56d5d6160968a0615f35
kimye702 <105279821+kimye702@users.noreply.github.com>
2023-08-19 00:43:11 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import  MainPage  from './MainPage';
+import { RegisterPage } from './Pages/RegisterPage';
+import { LoginPage } from './Pages/LoginPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<MainPage></MainPage>}></Route>
-{/*       <Route path='/main' element={<MainPage></MainPage>}></Route> */}
+      {/* <Route path='/main' element={<MainPage></MainPage>}></Route> */}
     </Routes>
   );
 }
