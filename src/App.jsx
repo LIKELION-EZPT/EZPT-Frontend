c9b58d8c9048da6957ad2f83848bf5a78f7a2016
kimye702 <105279821+kimye702@users.noreply.github.com>
2023-08-19 00:25:59 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<MainPage></MainPage>}></Route>
-      {/* <Route path='/main' element={<MainPage></MainPage>}></Route> */}
+      <Route path='/main' element={<MainPage></MainPage>}></Route>
     </Routes>
   );
 }
